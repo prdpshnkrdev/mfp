5097de05b2a0ec4771a0ba00f2372df58d8e857a
prdpshnkrdev <prdpcode@gmail.com>
2024-04-25 12:52:03 +0530

    fixes

--- container/src/components/AuthApp.js
+++ container/src/components/AuthApp.js
@@ -9,19 +9,17 @@ export default () => {
   useEffect(() => {
     const { onParentNavigate } = mount(ref.current, {
       initialPath: history.location.pathname,
-      onNavigate: ({ pathname: nextPathName }) => {
+      onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
-        if (pathname !== nextPathName) {
-          history.push(nextPathName);
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
         }
       },
     });
+
     history.listen(onParentNavigate);
   }, []);
 
-  return (
-    <div ref={ref}>
-      <h1>AuthApp</h1>
-    </div>
-  );
+  return <div ref={ref} />;
 };
--- container/src/components/Progress.js
+++ container/src/components/Progress.js
@@ -2,11 +2,11 @@ import React from "react";
 import { makeStyles, createStyles } from "@material-ui/core/styles";
 import LinearProgress from "@material-ui/core/LinearProgress";
 
-const useStyles = makeStyles(() => {
+const useStyles = makeStyles((theme) => {
   return createStyles({
     bar: {
       width: "100%",
-      "&>*+*": {
+      "& > * + *": {
         marginTop: theme.spacing(2),
       },
     },
